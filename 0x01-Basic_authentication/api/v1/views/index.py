3012aad6c3ddbdc456a6a5d508d3ee87d48280b8
kezzyj <karachidavid31@gmail.com>
2023-03-07 12:42:03 +0000

    setting up all

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,15 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized/", strict_slashes=False)
+def unauthorized() -> str:
+    """Tests the 401 error handler"""
+    abort(401)
+
+
+@app_views.route("/forbidden/", strict_slashes=False)
+def forbidden() -> str:
+    """Tests the 403 error handler"""
+    abort(403)
