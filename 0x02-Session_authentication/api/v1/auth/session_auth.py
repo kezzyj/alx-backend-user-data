8a54c053e6226f7e1f481c5380f99f46f86a3dee
kezzyj <karachidavid31@gmail.com>
2023-03-10 16:55:10 +0000

    others

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,33 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''returns a User ID based on a Session ID'''
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        '''returns a User instance based on a cookie value'''
+        cookie = self.session_cookie(request)
+        if cookie is None:
+            return None
+        user_id = self.user_id_for_session_id(cookie)
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        '''deletes the user session / logout'''
+        if request is None:
+            return False
+        if not self.session_cookie(request):
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        if self.user_id_by_session_id.get(session_id, None) is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
